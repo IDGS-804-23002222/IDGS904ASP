1be84bfb374dde1c82c2fdd8ddeacef7173bbbd4
IDGS-804-23002222 <karlamartinez2004@gmail.com>
2026-05-13 22:42:03 -0600

    Display 13/05/26

---
+++ IDGS904ASP/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using IDGS904ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDGS904ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*
+        public ActionResult Index()
+          {
+              return View();
+          }
+        */
+
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        /* 
+        // Devuelve solo texto
+        public ContentResult Index()
+        {
+            return Content("Karla IDGS905");
+        }
+        */
+
+        /*
+        // Devuelve siempre una vista
+        public ViewResult Index()
+        {
+            return View();
+        }
+        */
+
+        /*
+        // para regresar una cadena de texto json
+        public JsonResult Index()
+        {
+            var alumno1 = new Persona() { Nombre = "Karla", Edad = 21, Empleado = true };
+            var alumno2 = new Persona() { Nombre = "Paulo", Edad = 22, Empleado = false };
+            return Json(new List<Persona>() { alumno1, alumno2 }, JsonRequestBehavior.AllowGet);
+
+        }
+        */
+
+        /*
+        // redireccionar a otra pagina
+        public RedirectResult Index()
+        {
+            return Redirect("https://google.com.mx");
+        }
+        */
+
+        
+        // redireccionar a otro controlador
+        public RedirectToRouteResult Index()
+        {
+            return RedirectToAction("Contacto", "Pruebas");
+        }
+        
+    }
+}
---
+++ IDGS904ASP/Controllers/PruebasController.cs
@@ -0,0 +1,39 @@
+﻿using IDGS904ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDGS904ASP.Controllers
+{
+    public class PruebasController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contacto()
+        {
+            ViewBag.Nombre = "Karla";
+            ViewBag.Apellido = "Martinez";
+            ViewData["Materia"] = "DWI";
+            return View();
+        }
+
+        public ActionResult Empleado()
+        {
+            var persona = new Persona()
+            {
+                Nombre = "Karla",
+                Edad = 21,
+                Empleado = true,
+                Nacimiento = new DateTime(2004, 11, 21)
+            };
+            ViewBag.Propiedad = persona;
+            return View();
+        }
+    }
+}
