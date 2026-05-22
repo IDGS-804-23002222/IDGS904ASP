9b0f9367194570b323a995c578e9b5237ee03fe2
IDGS-804-23002222 <karlamartinez2004@gmail.com>
2026-05-21 18:54:37 -0600

    Cinepolis

--- IDGS904ASP/Controllers/CinepolisController.cs
+++ IDGS904ASP/Controllers/CinepolisController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IDGS904ASP.Models;
 
 namespace IDGS904ASP.Controllers
 {
@@ -11,7 +12,14 @@ namespace IDGS904ASP.Controllers
         // GET: Cinepolis
         public ActionResult Index()
         {
-            return View();
+            return View(new Cinepolis());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Cinepolis cinepolis)
+        {
+            cinepolis.CalcularPago();
+            return View(cinepolis); 
         }
     }
 }
---
+++ IDGS904ASP/Models/Cinepolis.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDGS904ASP.Models
+{
+    public class Cinepolis
+    {
+        public string Nombre { get; set; }
+        public int CantidadCompradores { get; set; }
+        public int CantidadBoletos { get; set; }
+        public bool TarjetaCineco { get; set; }
+        public double ValorPagar { get; set; }
+        public string Mensaje { get; set; }
+
+        public void CalcularPago()
+        {
+            double precioBoleto = 12.00;
+            int maxBoletos = CantidadCompradores * 7;
+
+            if (CantidadBoletos > maxBoletos)
+            {
+                Mensaje = "No se pueden comprar más de 7 boletos por persona.";
+                ValorPagar = 0;
+                return;
+            }
+
+            double subtotal = CantidadBoletos * precioBoleto;
+
+            double descuento = 0;
+            int porcentajeDescuento = 0;
+
+            // Descuento por cantidad de boletos
+            if (CantidadBoletos > 5)
+            {
+                porcentajeDescuento = 15;
+                descuento = subtotal * 0.15;
+            }
+            else if (CantidadBoletos >= 3 && CantidadBoletos <= 5)
+            {
+                porcentajeDescuento = 10;
+                descuento = subtotal * 0.10;
+            }
+
+            double total = subtotal - descuento;
+
+            // Descuento tarjeta Cineco
+            double descuentoTarjeta = 0;
+
+            if (TarjetaCineco)
+            {
+                descuentoTarjeta = total * 0.10;
+                total = total - descuentoTarjeta;
+            }
+
+            ValorPagar = total;
+
+            Mensaje =
+                "Subtotal: $" + subtotal.ToString("F2") + "\n" +
+                "Descuento aplicado: " + porcentajeDescuento + "%\n" +
+                "Cantidad descontada: $" + descuento.ToString("F2") + "\n";
+
+            if (TarjetaCineco)
+            {
+                Mensaje +=
+                    "Descuento adicional Cineco: $" +
+                    descuentoTarjeta.ToString("F2") + "\n";
+            }
+
+            Mensaje +=
+                "Total a pagar: $" + ValorPagar.ToString("F2");
+        }
+    }
+}
